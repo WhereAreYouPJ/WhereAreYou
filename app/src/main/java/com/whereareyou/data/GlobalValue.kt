9b6026b33ef3a5c27bcd56c9bdb81e2023483bf3
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-10-12 22:39:09 +0900

    Update

---
+++ app/src/main/java/com/whereareyou/data/GlobalValue.kt
@@ -0,0 +1,11 @@
+package com.whereareyou.data
+
+object GlobalValue {
+
+    var screenHeightWithoutStatusBar = 1f
+    var topAppBarHeight = 1f
+    var bottomNavBarHeight = 1f
+    var calendarViewHeight = 1f
+    var dailyScheduleViewHeight = 1f
+    var density = 1f
+}
