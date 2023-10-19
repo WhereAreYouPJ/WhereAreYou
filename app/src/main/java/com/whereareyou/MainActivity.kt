274c6a37eeb6a28d539fc5e34c29a3b166499469
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-10-19 10:49:41 +0900

    Update

--- app/src/main/java/com/whereareyou/MainActivity.kt
+++ app/src/main/java/com/whereareyou/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.whereareyou
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,18 +16,40 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.whereareyou.data.GlobalValue
 import com.whereareyou.ui.MainNavigation
 import com.whereareyou.ui.theme.WhereAreYouTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("InternalInsetResource", "DiscouragedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val metrics = resources.displayMetrics
+        val statusBarResourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        // getDimension(): dimen.xml에 정의한 dp값을 기기에 맞게 px로 변환하여 반올림한 값을 int로 반환한다.
+        val screenHeight = metrics.heightPixels
+        val screenWidth = metrics.widthPixels
+        val statusBarHeight = resources.getDimension(statusBarResourceId)
+        GlobalValue.screenHeightWithoutStatusBar = screenHeight.toFloat()
+        GlobalValue.bottomNavBarHeight = GlobalValue.screenHeightWithoutStatusBar / 15
+        GlobalValue.topAppBarHeight = GlobalValue.screenHeightWithoutStatusBar / 15
+        GlobalValue.calendarViewHeight = GlobalValue.screenHeightWithoutStatusBar * 26 / 75
+        GlobalValue.dailyScheduleViewHeight = GlobalValue.screenHeightWithoutStatusBar * 39 / 75
+        GlobalValue.density = metrics.density
+
+        Log.e("", "statusBarPadding : ${statusBarHeight}")
+        Log.e("", "metrics.widthPixels : $screenHeight $screenWidth")
+        Log.e("", "metrics.heightPixels : ${GlobalValue.screenHeightWithoutStatusBar}")
+        Log.e("", "metrics.heightPixels : ${GlobalValue.bottomNavBarHeight}")
+        Log.e("", "metrics.heightPixels : ${GlobalValue.topAppBarHeight}")
+        Log.e("", "metrics.heightPixels : ${GlobalValue.calendarViewHeight}")
+        Log.e("", "metrics.heightPixels : ${GlobalValue.dailyScheduleViewHeight}")
+
         setContent {
             WhereAreYouTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier
                         .fillMaxSize(),
