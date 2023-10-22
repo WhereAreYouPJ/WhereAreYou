dc51783b171188eb81182b2dddbae8a916dbfe40
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-10-23 00:41:01 +0900

    Update

--- app/src/main/java/com/whereareyou/di/RemoteDataSourceModule.kt
+++ app/src/main/java/com/whereareyou/di/RemoteDataSourceModule.kt
@@ -1,7 +1,9 @@
 package com.whereareyou.di
 
+import com.whereareyou.api.SearchLocationApi
 import com.whereareyou.api.WhereAreYouApi
 import com.whereareyou.datasource.RemoteDataSource
+import com.whereareyou.datasource.SearchLocationDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,5 +20,13 @@ object RemoteDataSourceModule {
         api: WhereAreYouApi
     ): RemoteDataSource {
         return RemoteDataSource(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSearchLocationDataSource(
+        api: SearchLocationApi
+    ): SearchLocationDataSource {
+        return SearchLocationDataSource(api)
     }
 }
