4ea861fba18b80fdeaa3862650ee0eb195a56a20
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-10-27 02:00:40 +0900

    Update

--- app/src/main/java/com/whereareyou/di/usecase/SignInUseCaseModule.kt
+++ app/src/main/java/com/whereareyou/di/usecase/SignInUseCaseModule.kt
@@ -1,6 +1,8 @@
 package com.whereareyou.di.usecase
 
 import com.whereareyou.domain.repository.SignInRepository
+import com.whereareyou.domain.usecase.signin.GetAccessTokenUseCase
+import com.whereareyou.domain.usecase.signin.SaveAccessTokenUseCase
 import com.whereareyou.domain.usecase.signin.SignInUseCase
 import dagger.Module
 import dagger.Provides
@@ -16,5 +18,19 @@ object SignInUseCaseModule {
         repository: SignInRepository
     ): SignInUseCase {
         return SignInUseCase(repository)
+    }
+
+    @Provides
+    fun provideSaveAccessTokenUseCase(
+        repository: SignInRepository
+    ): SaveAccessTokenUseCase {
+        return SaveAccessTokenUseCase(repository)
+    }
+
+    @Provides
+    fun provideGetAccessTokenUseCase(
+        repository: SignInRepository
+    ): GetAccessTokenUseCase {
+        return GetAccessTokenUseCase(repository)
     }
 }
