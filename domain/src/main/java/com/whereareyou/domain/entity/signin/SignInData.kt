2a84de5409083a401ece4d52ee960b0585f61ece
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-03 00:25:06 +0900

    Update

--- domain/src/main/java/com/whereareyou/domain/entity/signin/SignInData.kt
+++ domain/src/main/java/com/whereareyou/domain/entity/signin/SignInData.kt
@@ -3,8 +3,6 @@ package com.whereareyou.domain.entity.signin
 import com.google.gson.annotations.SerializedName
 
 data class SignInData(
-    @SerializedName("message")
-    val message: String,
     @SerializedName("accessToken")
     val accessToken: String,
     @SerializedName("refreshToken")
