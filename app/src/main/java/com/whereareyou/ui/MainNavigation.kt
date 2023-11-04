1b9c71d0f59990de35a2f33bd9464265967d7cc8
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-04 13:00:49 +0900

    Update

--- app/src/main/java/com/whereareyou/ui/MainNavigation.kt
+++ app/src/main/java/com/whereareyou/ui/MainNavigation.kt
@@ -3,27 +3,44 @@ package com.whereareyou.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.Navigator
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.whereareyou.GlobalViewModel
 import com.whereareyou.data.Constants
 import com.whereareyou.ui.home.HomeScreen
 import com.whereareyou.ui.home.main.detailschedule.DetailScheduleScreen
 import com.whereareyou.ui.home.main.newschedule.NewScheduleContent
 import com.whereareyou.ui.signin.SignUpScreen
+import com.whereareyou.ui.splash.SplashScreen
 
 @Composable
 fun MainNavigation(
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController = rememberNavController(),
+    viewModel: GlobalViewModel = hiltViewModel()
 ) {
     NavHost(
         modifier = Modifier
             .fillMaxSize(),
         navController = navController,
-        startDestination = Constants.ROUTE_MAIN_HOME
+        startDestination = Constants.ROUTE_MAIN_SPLASH
     ) {
+        composable(
+            route = Constants.ROUTE_MAIN_SPLASH
+        ) {
+            SplashScreen(
+                checkIsSignedIn = { viewModel.checkIsSignedIn() },
+                moveToSignInScreen = {
+                },
+                moveToMainScreen = {
+                    navController.navigate(Constants.ROUTE_MAIN_HOME)
+                }
+            )
+        }
+
         composable(
             route = Constants.ROUTE_MAIN_SIGNUP
         ) {
