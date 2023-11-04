f87b1b86217d40ecb9e0a7b6ca16d75f72a031cc
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-04 13:00:38 +0900

    Update

--- app/src/main/java/com/whereareyou/ui/home/main/detailschedule/DetailScheduleScreen.kt
+++ app/src/main/java/com/whereareyou/ui/home/main/detailschedule/DetailScheduleScreen.kt
@@ -1,10 +1,12 @@
 package com.whereareyou.ui.home.main.detailschedule
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -15,13 +17,20 @@ fun DetailScheduleScreen(
     LaunchedEffect(true) { viewModel.updateScheduleId(scheduleId) }
 
     Column(
-
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         Text(
-            text = "${scheduleId}" +
-                    "\n${viewModel.title.collectAsState().value}" +
-                    "\n${viewModel.start.collectAsState().value}" +
-                    "\n${viewModel.end.collectAsState().value}"
+            text = "제목 : ${viewModel.title.collectAsState().value}"
+        )
+        Text(
+            text = "시작 시간 : ${viewModel.start.collectAsState().value}"
+        )
+        Text(
+            text = "종료 시간 : ${viewModel.end.collectAsState().value}"
+        )
+        Text(
+            text = "장소 : ${viewModel.place.collectAsState().value}"
         )
 
     }
