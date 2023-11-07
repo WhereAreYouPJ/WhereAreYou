0caa5536def088cf26e380d207d2c86f1f6834c8
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-07 15:44:36 +0900

    Update

--- data/src/main/java/com/whereareyou/repository/ScheduleRepositoryImpl.kt
+++ data/src/main/java/com/whereareyou/repository/ScheduleRepositoryImpl.kt
@@ -1,7 +1,17 @@
 package com.whereareyou.repository
 
 import android.util.Log
+import com.whereareyou.domain.entity.apimessage.schedule.AddNewScheduleRequest
 import com.whereareyou.datasource.RemoteDataSource
+import com.whereareyou.domain.entity.apimessage.schedule.AcceptScheduleRequest
+import com.whereareyou.domain.entity.apimessage.schedule.AddNewScheduleResponse
+import com.whereareyou.domain.entity.apimessage.schedule.CheckArrivalRequest
+import com.whereareyou.domain.entity.apimessage.schedule.DeleteScheduleRequest
+import com.whereareyou.domain.entity.apimessage.schedule.EndScheduleRequest
+import com.whereareyou.domain.entity.apimessage.schedule.GetDailyBriefScheduleResponse
+import com.whereareyou.domain.entity.apimessage.schedule.GetDetailScheduleResponse
+import com.whereareyou.domain.entity.apimessage.schedule.GetMonthlyScheduleResponse
+import com.whereareyou.domain.entity.apimessage.schedule.ModifyScheduleRequest
 import com.whereareyou.domain.entity.schedule.BriefSchedule
 import com.whereareyou.domain.entity.schedule.DetailSchedule
 import com.whereareyou.domain.entity.schedule.ScheduleCountByDay
@@ -15,58 +25,135 @@ class ScheduleRepositoryImpl(
     private val dataSource: RemoteDataSource
 ) : ScheduleRepository, NetworkResultHandler {
 
+    /**
+     * 월별 일정 정보
+     * implements [ScheduleRepository.getMonthlySchedule]
+     */
     override suspend fun getMonthlySchedule(
         token: String,
         memberId: String,
         year: Int,
         month: Int
-    ): NetworkResult<List<ScheduleCountByDay>> {
-        val response = dataSource.getMonthlySchedule(token, memberId, year, month)
-        Log.e("getMonthlySchedule", "getMonthlySchedule")
+    ): NetworkResult<GetMonthlyScheduleResponse> {
         return withContext(Dispatchers.IO) {
-            handleResult(response) { body ->
-                body.schedules
-            }
+            val response = dataSource.getMonthlySchedule(token, memberId, year, month)
+            handleResult(response) { it }
         }
     }
 
+    /**
+     * 일별 간략 정보
+     * implements [ScheduleRepository.getDailyBriefSchedule]
+     */
     override suspend fun getDailyBriefSchedule(
         token: String,
         memberId: String,
         year: Int,
         month: Int,
         date: Int
-    ): NetworkResult<List<BriefSchedule>> {
-        val response = dataSource.getDailyBriefSchedule(token, memberId, year, month, date)
+    ): NetworkResult<GetDailyBriefScheduleResponse> {
         return withContext(Dispatchers.IO) {
-            handleResult(response) { body ->
-                body.schedules
-            }
+            val response = dataSource.getDailyBriefSchedule(token, memberId, year, month, date)
+            handleResult(response) { it }
         }
     }
 
+    /**
+     * 일별 일정 상세 정보
+     * implements [ScheduleRepository.getDetailSchedule]
+     */
     override suspend fun getDetailSchedule(
         token: String,
         memberId: String,
         scheduleId: String
-    ): NetworkResult<DetailSchedule> {
-        val response = dataSource.getDetailSchedule(token, memberId, scheduleId)
+    ): NetworkResult<GetDetailScheduleResponse> {
         return withContext(Dispatchers.IO) {
-            handleResult(response) { body ->
-                DetailSchedule(body.start, body.end, body.title, body.place, body.memo, body.friendsIdList) }
+            val response = dataSource.getDetailSchedule(token, memberId, scheduleId)
+            handleResult(response) { it }
         }
     }
 
+    /**
+     * 일정 추가
+     * implements [ScheduleRepository.addNewSchedule]
+     */
     override suspend fun addNewSchedule(
         token: String,
-        memberId: String,
-        start: String,
-        end: String,
-        title: String,
-        place: String,
-        momo: String,
-        memberIdList: List<String>
-    ): NetworkResult<String> {
-        TODO("Not yet implemented")
+        body: AddNewScheduleRequest
+    ): NetworkResult<AddNewScheduleResponse> {
+        return withContext(Dispatchers.IO) {
+            val response = dataSource.addNewSchedule(token, body)
+            handleResult(response) { it }
+        }
+    }
+
+    /**
+     * 일정 수정
+     * implements [ScheduleRepository.modifySchedule]
+     */
+    override suspend fun modifySchedule(
+        token: String,
+        body: ModifyScheduleRequest
+    ): NetworkResult<Nothing> {
+        return withContext(Dispatchers.IO) {
+            val response = dataSource.modifySchedule(token, body)
+            handleResult(response) { it }
+        }
+    }
+
+    /**
+     * 일정 삭제
+     * implements [ScheduleRepository.deleteSchedule]
+     */
+    override suspend fun deleteSchedule(
+        token: String,
+        body: DeleteScheduleRequest
+    ): NetworkResult<Nothing> {
+        return withContext(Dispatchers.IO) {
+            val response = dataSource.deleteSchedule(token, body)
+            handleResult(response) { it }
+        }
+    }
+
+    /**
+     * 일정 수락
+     * implements [ScheduleRepository.acceptSchedule]
+     */
+    override suspend fun acceptSchedule(
+        token: String,
+        body: AcceptScheduleRequest
+    ): NetworkResult<Boolean> {
+        return withContext(Dispatchers.IO) {
+            val response = dataSource.acceptSchedule(token, body)
+            handleResult(response) { it }
+        }
+    }
+
+    /**
+     * 일정 종료
+     * implements [ScheduleRepository.endSchedule]
+     */
+    override suspend fun endSchedule(
+        token: String,
+        body: EndScheduleRequest
+    ): NetworkResult<Boolean> {
+        return withContext(Dispatchers.IO) {
+            val response = dataSource.endSchedule(token, body)
+            handleResult(response) { it }
+        }
+    }
+
+    /**
+     * 도착 여부
+     * implements [ScheduleRepository.checkArrival]
+     */
+    override suspend fun checkArrival(
+        token: String,
+        body: CheckArrivalRequest
+    ): NetworkResult<Boolean> {
+        return withContext(Dispatchers.IO) {
+            val response = dataSource.checkArrival(token, body)
+            handleResult(response) { it }
+        }
     }
 }
