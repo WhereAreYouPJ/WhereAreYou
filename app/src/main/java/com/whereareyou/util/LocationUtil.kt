11ecc8d0a8741b2e9bb8e799a103a5dfab24a324
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-18 00:09:58 +0900

    Create

---
+++ app/src/main/java/com/whereareyou/util/LocationUtil.kt
@@ -0,0 +1,66 @@
+package com.whereareyou.util
+
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.naver.maps.geometry.LatLng
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class LocationUtil @Inject constructor(
+    private val context: Context
+) {
+    fun getCurrentLocation(latLng: Coordinate) {
+        val request = LocationRequest.Builder(10000L)
+            .setIntervalMillis(10000L)
+            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+            .build()
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.locations.lastOrNull()?.let {
+                    latLng.latitude = it.latitude
+                    latLng.longitude = it.longitude
+                }
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        LocationServices.getFusedLocationProviderClient(context).requestLocationUpdates(
+            request,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+}
+
+data class Coordinate(
+    var latitude: Double,
+    var longitude: Double
+)
