a1d7323ca1664924079e00692e718f167488bf8b
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-19 01:26:15 +0900

    Update

--- app/src/main/java/com/whereareyou/data/GlobalValue.kt
+++ app/src/main/java/com/whereareyou/data/GlobalValue.kt
@@ -2,10 +2,16 @@ package com.whereareyou.data
 
 object GlobalValue {
 
+    // 상단 상태바, 시스템 네비게이션 바 제외한 화면 높이
     var screenHeightWithoutStatusBar = 1f
+    // 상단 영역 높이
     var topAppBarHeight = 1f
+    // 하단 네비게이션 바 높이
     var bottomNavBarHeight = 1f
+    // 캘린더 뷰 높이
     var calendarViewHeight = 1f
+    // 일별 간략 정보 뷰 높이
     var dailyScheduleViewHeight = 1f
+    // pixel <-> dp 전환에 사용할 밀도
     var density = 1f
 }
