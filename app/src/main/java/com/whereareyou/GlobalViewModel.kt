5a86edae450efc0245158f2b64128b7f260a975f
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-19 01:26:23 +0900

    Update

--- app/src/main/java/com/whereareyou/GlobalViewModel.kt
+++ app/src/main/java/com/whereareyou/GlobalViewModel.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
@@ -17,6 +18,8 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.naver.maps.geometry.LatLng
 import com.whereareyou.data.GlobalValue
 import com.whereareyou.domain.entity.apimessage.location.SendUserLocationRequest
@@ -29,6 +32,9 @@ import com.whereareyou.domain.usecase.signin.SaveMemberIdUseCase
 import com.whereareyou.domain.usecase.signin.SignInUseCase
 import com.whereareyou.domain.util.NetworkResult
 import com.whereareyou.repository.SharedPreferencesRepository
+import com.whereareyou.util.CalendarUtil
+import com.whereareyou.util.Coordinate
+import com.whereareyou.util.LocationUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
@@ -44,13 +50,15 @@ class GlobalViewModel @Inject constructor(
     private val saveMemberIdUseCase: SaveMemberIdUseCase,
     private val getMemberIdUseCase: GetMemberIdUseCase,
     private val sendUserLocationUseCase: SendUserLocationUseCase,
+    private val locationUtil: LocationUtil,
     application: Application
 ) : AndroidViewModel(application) {
 
+    private var currentLocation = Coordinate(0.0, 0.0)
     // 임시 로그인
     private fun signIn() {
         viewModelScope.launch {
-            val request = SignInRequest("wnstjd00", "00")
+            val request = SignInRequest("user1", "00")
             val signInResult = signInUseCase(request)
             when (signInResult) {
                 is NetworkResult.Success -> {
@@ -67,26 +75,6 @@ class GlobalViewModel @Inject constructor(
         }
     }
 
-    @SuppressLint("DiscouragedApi", "InternalInsetResource")
-    fun updateGlobalValue() {
-
-        // 모든 수치는 pixel단위로 저장된다
-
-        val resources = getApplication<Application>().resources
-        val metrics = resources.displayMetrics
-        val statusBarResourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-        // getDimension(): dimen.xml에 정의한 dp값을 기기에 맞게 px로 변환하여 반올림한 값을 int로 반환한다.
-        val screenHeight = metrics.heightPixels
-        val screenWidth = metrics.widthPixels
-        val statusBarHeight = resources.getDimension(statusBarResourceId)
-        GlobalValue.screenHeightWithoutStatusBar = screenHeight.toFloat()
-        GlobalValue.bottomNavBarHeight = GlobalValue.screenHeightWithoutStatusBar / 15
-        GlobalValue.topAppBarHeight = GlobalValue.screenHeightWithoutStatusBar / 15
-        GlobalValue.calendarViewHeight = GlobalValue.screenHeightWithoutStatusBar * 26 / 75
-        GlobalValue.dailyScheduleViewHeight = GlobalValue.screenHeightWithoutStatusBar * 39 / 75
-        GlobalValue.density = metrics.density
-    }
-
     fun checkIsSignedIn(): Boolean {
         var isSignedIn = false
         runBlocking {
@@ -98,50 +86,21 @@ class GlobalViewModel @Inject constructor(
         return isSignedIn
     }
 
-    fun getLocation() {
-        val request = LocationRequest.Builder(10000L)
-            .setIntervalMillis(10000L)
-            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
-            .build()
-
-        val locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult) {
-                locationResult.locations.lastOrNull()?.let {
-                    LatLng(it.latitude, it.longitude)
-                    sendUserLocation(it)
-//                    Log.e("location", "$it")
-                }
+    private fun getLocation() {
+        viewModelScope.launch {
+            while (true) {
+                delay(20000)
+                Log.e("GlobalViewModel", "${currentLocation.latitude}, ${currentLocation.longitude}")
+                sendUserLocation(currentLocation.latitude, currentLocation.longitude)
             }
         }
-        if (ActivityCompat.checkSelfPermission(
-                getApplication(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                getApplication(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        LocationServices.getFusedLocationProviderClient(getApplication<Application>().applicationContext).requestLocationUpdates(
-            request,
-            locationCallback,
-            Looper.getMainLooper()
-        )
     }
 
-    fun sendUserLocation(location: Location) {
+    fun sendUserLocation(lat: Double, lng: Double) {
         viewModelScope.launch {
             val accessToken = getAccessTokenUseCase().first()
             val memberId = getMemberIdUseCase().first()
-            val request = SendUserLocationRequest(memberId, location.latitude, location.longitude)
+            val request = SendUserLocationRequest(memberId, lat, lng)
             val result = sendUserLocationUseCase(accessToken, request)
             when (result) {
                 is NetworkResult.Success -> {
@@ -153,8 +112,25 @@ class GlobalViewModel @Inject constructor(
         }
     }
 
+    fun getToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e("GlobalViewModel-token", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            val token = task.result
+            Log.e("GlobalViewModel-token", token)
+
+            val msg = token.toString()
+            Toast.makeText(getApplication(), msg, Toast.LENGTH_SHORT).show()
+        })
+    }
+
     init {
+        locationUtil.getCurrentLocation(latLng = currentLocation)
         signIn()
         getLocation()
+        getToken()
     }
 }
