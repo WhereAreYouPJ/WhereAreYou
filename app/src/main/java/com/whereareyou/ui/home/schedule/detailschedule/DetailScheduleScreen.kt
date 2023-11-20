d2abbde796c8150a79a3979026f14fa66da122d8
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-20 22:13:50 +0900

    Update

---
+++ app/src/main/java/com/whereareyou/ui/home/schedule/detailschedule/DetailScheduleScreen.kt
@@ -0,0 +1,26 @@
+package com.whereareyou.ui.home.schedule.detailschedule
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun DetailScheduleScreen(
+    scheduleId: String?,
+    viewModel: DetailScheduleViewModel = hiltViewModel()
+) {
+    LaunchedEffect(true) { viewModel.updateScheduleId(scheduleId) }
+    when (viewModel.screenState.collectAsState().value) {
+        DetailScheduleViewModel.ScreenState.DetailSchedule -> {
+            DetailScheduleContent(
+                moveToUserMapScreen = { viewModel.updateScreenState(DetailScheduleViewModel.ScreenState.UserMap) }
+            )
+        }
+        DetailScheduleViewModel.ScreenState.UserMap -> {
+            UserMapScreen(
+                moveToDetailScheduleScreen = { viewModel.updateScreenState(DetailScheduleViewModel.ScreenState.DetailSchedule) }
+            )
+        }
+    }
+}
