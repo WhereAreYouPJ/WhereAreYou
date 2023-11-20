c5fc5ee161a4aea31c614820771415b33fff0ea3
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-21 00:03:34 +0900

    Update

---
+++ app/src/main/java/com/whereareyou/ui/home/schedule/notification/DrawerNotificationViewModel.kt
@@ -0,0 +1,15 @@
+package com.whereareyou.ui.home.schedule.notification
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DrawerNotificationViewModel @Inject constructor(
+    private val application: Application
+) : AndroidViewModel(application) {
+    fun loadRequests() {
+
+    }
+}
