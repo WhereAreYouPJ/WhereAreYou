54f3cf2287149882d02f637fe79f92b1d911f2ed
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-22 10:20:57 +0900

    Update

--- app/src/main/java/com/whereareyou/data/GlobalValue.kt
+++ app/src/main/java/com/whereareyou/data/GlobalValue.kt
@@ -7,13 +7,13 @@ object GlobalValue {
     // 화면 가로 너비
     var screenWidth = 720f
     // 상단 영역 높이
-    var topAppBarHeight = 1f
+    var topAppBarHeight = 97.6f
     // 하단 네비게이션 바 높이
-    var bottomNavBarHeight = 1f
+    var bottomNavBarHeight = 97.6f
     // 캘린더 뷰 높이
-    var calendarViewHeight = 1f
+    var calendarViewHeight = 507.52f
     // 일별 간략 정보 뷰 높이
-    var dailyScheduleViewHeight = 1f
+    var dailyScheduleViewHeight = 761.28f
     // pixel <-> dp 전환에 사용할 밀도
     var density = 1f
 }
