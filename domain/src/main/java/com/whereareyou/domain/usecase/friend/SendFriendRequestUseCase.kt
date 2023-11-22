49c9a08f4340cb684dc558deb86cdc69030987c2
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-22 18:53:58 +0900

    Update

---
+++ domain/src/main/java/com/whereareyou/domain/usecase/friend/SendFriendRequestUseCase.kt
@@ -0,0 +1,16 @@
+package com.whereareyou.domain.usecase.friend
+
+import com.whereareyou.domain.entity.apimessage.friend.SendFriendRequestRequest
+import com.whereareyou.domain.repository.FriendRepository
+import com.whereareyou.domain.util.NetworkResult
+
+class SendFriendRequestUseCase(
+    private val repository: FriendRepository
+) {
+    suspend operator fun invoke(
+        token: String,
+        body: SendFriendRequestRequest
+    ): NetworkResult<Nothing> {
+        return repository.sendFriendRequest(token, body)
+    }
+}
