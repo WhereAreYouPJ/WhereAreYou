86fc00cc29405637d7007098f72b1138957765ee
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-24 20:38:42 +0900

    Update

--- app/src/main/java/com/whereareyou/data/Friend.kt
+++ app/src/main/java/com/whereareyou/data/Friend.kt
@@ -4,6 +4,6 @@ data class Friend(
     val number: Int,
     val name: String = "",
     val id: String = "",
-    val profileImgUrl: String = "",
+    val profileImgUrl: String? = "",
     val isPinned: Boolean = true
 )
