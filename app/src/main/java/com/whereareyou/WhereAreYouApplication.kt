155d694d5eeec051ee3ead1721fa6fef101544cd
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-27 19:21:28 +0900

    Update

--- app/src/main/java/com/whereareyou/WhereAreYouApplication.kt
+++ app/src/main/java/com/whereareyou/WhereAreYouApplication.kt
@@ -1,9 +1,17 @@
 package com.whereareyou
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class WhereAreYouApplication : Application() {
+    init {
+        instance = this
+    }
 
+    companion object {
+        lateinit var instance: WhereAreYouApplication
+        fun applicationContext(): Context = instance.applicationContext
+    }
 }
