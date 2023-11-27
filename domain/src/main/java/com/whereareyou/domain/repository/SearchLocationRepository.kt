aa21d7786821f91ef33e4df822a63a834d1d25b7
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-11-27 19:22:52 +0900

    Update

--- domain/src/main/java/com/whereareyou/domain/repository/SearchLocationRepository.kt
+++ domain/src/main/java/com/whereareyou/domain/repository/SearchLocationRepository.kt
@@ -1,11 +1,11 @@
 package com.whereareyou.domain.repository
 
-import com.whereareyou.domain.entity.schedule.LocationInformation
+import com.whereareyou.domain.entity.apimessage.schedule.GetLocationAddressResponse
 import com.whereareyou.domain.util.NetworkResult
 
 interface SearchLocationRepository {
 
     suspend fun getLocationAddress(
         query: String
-    ): NetworkResult<List<LocationInformation>>
+    ): NetworkResult<GetLocationAddressResponse>
 }
