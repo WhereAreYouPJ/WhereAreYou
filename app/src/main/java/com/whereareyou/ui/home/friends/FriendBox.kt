265624b3f8f713446a12302993ab90954662657d
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-12-02 02:24:16 +0900

    Update FriendBox.kt

--- app/src/main/java/com/whereareyou/ui/home/friends/FriendBox.kt
+++ app/src/main/java/com/whereareyou/ui/home/friends/FriendBox.kt
@@ -1,6 +1,7 @@
 package com.whereareyou.ui.home.friends
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -9,36 +10,42 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.glide.GlideImage
+import com.whereareyou.R
 
 @Composable
 fun FriendBox(
-    imageUrl: String,
+    imageUrl: String?,
     friendName: String
 ) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 10.dp, bottom = 10.dp)
+            .padding(top = 10.dp, bottom = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         GlideImage(
             modifier = Modifier
                 .width(40.dp)
                 .height(40.dp)
                 .clip(shape = RoundedCornerShape(50)),
-            imageModel = { imageUrl },
+            imageModel = { imageUrl ?: R.drawable.account_circle_fill0_wght200_grad0_opsz24 },
             imageOptions = ImageOptions(
                 contentScale = ContentScale.FillHeight,
             )
         )
+        Spacer(modifier = Modifier.width(10.dp))
         Text(
-            text = friendName
+            text = friendName,
+            fontSize = 20.sp
         )
     }
 }
