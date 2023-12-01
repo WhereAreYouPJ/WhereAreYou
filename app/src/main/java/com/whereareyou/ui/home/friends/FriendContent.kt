67f56b9585b669729bf2ab62e53bc9a8b850bf6f
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-12-02 02:24:19 +0900

    Update FriendContent.kt

--- app/src/main/java/com/whereareyou/ui/home/friends/FriendContent.kt
+++ app/src/main/java/com/whereareyou/ui/home/friends/FriendContent.kt
@@ -2,6 +2,7 @@ package com.whereareyou.ui.home.friends
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,11 +12,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.whereareyou.R
 import com.whereareyou.data.FriendProvider
 import com.whereareyou.domain.entity.schedule.Friend
@@ -35,9 +38,21 @@ fun FriendContent(
 //        )
 //    }
     LazyColumn() {
+        item {
+            Box(
+                modifier = Modifier
+                    .height(40.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = "친구 ${friendsList.size}",
+                    fontSize = 20.sp
+                )
+            }
+        }
         itemsIndexed(friendsList) { _, friend ->
             FriendBox(
-                imageUrl = "https://c8.alamy.com/comp/GJKAJ6/isolated-abstract-round-shape-green-color-plant-vector-logo-wheat-GJKAJ6.jpg",
+                imageUrl = friend.profileImgUrl,
                 friendName = friend.name
             )
 //            Text(
