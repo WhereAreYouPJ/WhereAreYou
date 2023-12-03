65516b72e54eeb943c4c0564284de4147e1058ca
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-12-04 00:53:36 +0900

    Update ScheduleScreen.kt

--- app/src/main/java/com/whereareyou/ui/home/schedule/calendar/ScheduleScreen.kt
+++ app/src/main/java/com/whereareyou/ui/home/schedule/calendar/ScheduleScreen.kt
@@ -3,6 +3,7 @@ package com.whereareyou.ui.home.schedule.calendar
 import android.util.Log
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.AnchoredDraggableState
@@ -21,10 +22,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
@@ -41,13 +44,18 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.whereareyou.R
 import com.whereareyou.data.GlobalValue
 import com.whereareyou.ui.home.schedule.notification.DrawerNotification
 import com.whereareyou.ui.home.schedule.notification.DrawerNotificationViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.Calendar
 import kotlin.math.roundToInt
@@ -65,17 +73,23 @@ fun ScheduleScreen(
     viewModel: CalendarViewModel = hiltViewModel(),
     notificationViewModel: DrawerNotificationViewModel = hiltViewModel(),
 ) {
+    val date = viewModel.date.collectAsState().value
+
     // 사이드바가 오른쪽에서 열리게 하기 위한 Rtl
     CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
         val drawerState = rememberDrawerState(
             initialValue = DrawerValue.Closed,
             confirmStateChange = {
                 notificationViewModel.loadFriendRequests()
+                notificationViewModel.loadScheduleRequests()
                 true
             }
         )
         ModalNavigationDrawer(
-            drawerContent = { DrawerNotification() },
+            drawerContent = { DrawerNotification(
+                updateCalendar = viewModel::updateCurrentMonthDateInfo,
+                updateBriefCalendar = { viewModel.updateDate(date) }
+            ) },
             drawerState = drawerState,
         ) {
             CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
@@ -107,7 +121,62 @@ fun ScheduleScreen(
 }
 
 @OptIn(ExperimentalFoundationApi::class)
-val anchoredDraggableState = AnchoredDraggableState(
+@Composable
+fun TopBar(
+    drawerState: DrawerState,
+    bottomContentState: AnchoredDraggableState<DetailState>,
+    viewModel: CalendarViewModel = hiltViewModel()
+) {
+    val density = LocalDensity.current
+    val calendarState = viewModel.calendarState.collectAsState().value
+    val year = viewModel.year.collectAsState().value
+    val month = viewModel.month.collectAsState().value
+    val coroutineScope = rememberCoroutineScope()
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height((GlobalValue.topBarHeight / density.density).dp)
+            .background(color = Color(0xFFCE93D8)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.clickable {
+                    coroutineScope.launch(Dispatchers.Default) { bottomContentState.animateTo(DetailState.Close) }
+                    viewModel.updateCalendarState(CalendarViewModel.CalendarState.YEAR)
+                },
+            text = "${year}.",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        if (calendarState == CalendarViewModel.CalendarState.DATE) {
+            Text(
+                modifier = Modifier.clickable {
+                        coroutineScope.launch { bottomContentState.animateTo(DetailState.Close) }
+                        viewModel.updateCalendarState(CalendarViewModel.CalendarState.MONTH)
+                    },
+                text = "${month}",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(((GlobalValue.topBarHeight / 2) / density.density).dp)
+                    .clickable { coroutineScope.launch(Dispatchers.Default) { drawerState.open() } },
+                painter = painterResource(id = R.drawable.notifications_fill0_wght200_grad0_opsz24),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+private val anchoredDraggableState = AnchoredDraggableState(
     initialValue = DetailState.Open,
     positionalThreshold = { it: Float -> it * 0.5f },
     velocityThreshold = { 100f },
