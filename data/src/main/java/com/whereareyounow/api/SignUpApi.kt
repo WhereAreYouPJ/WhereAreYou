3ccab6eb750342aec6ed35bb576e4b2c9a62a030
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-12-09 11:55:59 +0900

    Update SignUpApi.kt

--- data/src/main/java/com/whereareyounow/api/SignUpApi.kt
+++ data/src/main/java/com/whereareyounow/api/SignUpApi.kt
@@ -1,13 +1,10 @@
 package com.whereareyounow.api
 
 import com.whereareyounow.domain.entity.apimessage.signup.AuthenticateEmailCodeRequest
-import com.whereareyounow.domain.entity.apimessage.signup.AuthenticateEmailCodeResponse
 import com.whereareyounow.domain.entity.apimessage.signup.AuthenticateEmailRequest
-import com.whereareyounow.domain.entity.apimessage.signup.AuthenticateEmailResponse
 import com.whereareyounow.domain.entity.apimessage.signup.CheckEmailDuplicateResponse
 import com.whereareyounow.domain.entity.apimessage.signup.CheckIdDuplicateResponse
 import com.whereareyounow.domain.entity.apimessage.signup.SignUpRequest
-import com.whereareyounow.domain.entity.apimessage.signup.SignUpResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -28,21 +25,21 @@ interface SignUpApi {
         @Query("email") email: String
     ): Response<CheckEmailDuplicateResponse>
 
-    // 이메일 인증
+    // 이메일 인증 코드 전송
     @POST("member/email/send")
     suspend fun authenticateEmail(
         @Body body: AuthenticateEmailRequest
-    ): Response<AuthenticateEmailResponse>
+    ): Response<Unit>
 
     // 이메일 인증 코드 입력
     @POST("member/email/verify")
     suspend fun authenticateEmailCode(
         @Body body: AuthenticateEmailCodeRequest
-    ): Response<AuthenticateEmailCodeResponse>
+    ): Response<Unit>
 
     // 회원가입 성공
     @POST("member/join")
     suspend fun signUp(
         @Body body: SignUpRequest
-    ): Response<SignUpResponse>
+    ): Response<Unit>
 }
