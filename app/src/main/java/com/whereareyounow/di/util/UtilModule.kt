92d38ffda36888df11b9bc958b22e9382b5f27f5
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-12-10 10:49:43 +0900

    Update UtilModule.kt

--- app/src/main/java/com/whereareyounow/di/util/UtilModule.kt
+++ app/src/main/java/com/whereareyounow/di/util/UtilModule.kt
@@ -1,12 +1,19 @@
 package com.whereareyounow.di.util
 
 import android.content.Context
+import com.whereareyounow.domain.usecase.signin.GetAccessTokenUseCase
+import com.whereareyounow.domain.usecase.signin.GetRefreshTokenUseCase
+import com.whereareyounow.domain.usecase.signin.ReissueTokenUseCase
+import com.whereareyounow.domain.usecase.signin.SaveAccessTokenUseCase
+import com.whereareyounow.domain.usecase.signin.SaveRefreshTokenUseCase
 import com.whereareyounow.util.LocationUtil
+import com.whereareyounow.util.TokenManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,5 +24,25 @@ object UtilModule {
         @ApplicationContext context: Context
     ): LocationUtil {
         return LocationUtil(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTokenManager(
+        getAccessTokenUseCase: GetAccessTokenUseCase,
+        getRefreshTokenUseCase: GetRefreshTokenUseCase,
+        saveAccessTokenUseCase: SaveAccessTokenUseCase,
+        saveRefreshTokenUseCase: SaveRefreshTokenUseCase,
+        reissueTokenUseCase: ReissueTokenUseCase,
+        @ApplicationContext context: Context
+    ): TokenManager {
+        return TokenManager(
+            getAccessTokenUseCase,
+            getRefreshTokenUseCase,
+            saveAccessTokenUseCase,
+            saveRefreshTokenUseCase,
+            reissueTokenUseCase,
+            context
+        )
     }
 }
