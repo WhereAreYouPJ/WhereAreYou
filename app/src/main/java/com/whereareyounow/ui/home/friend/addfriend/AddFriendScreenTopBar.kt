ab3f97a347a7ae90fa380020d394cce8dd8a4c09
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-12-22 00:47:09 +0900

    Create AddFriendScreenTopBar.kt

---
+++ app/src/main/java/com/whereareyounow/ui/home/friend/addfriend/AddFriendScreenTopBar.kt
@@ -0,0 +1,65 @@
+package com.whereareyounow.ui.home.friend.addfriend
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.whereareyounow.R
+import com.whereareyounow.data.GlobalValue
+
+@Composable
+fun AddFriendScreenTopBar(
+    moveToBackScreen: () -> Unit
+) {
+    val density = LocalDensity.current.density
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height((GlobalValue.topBarHeight / density).dp)
+            .padding(start = 20.dp, end = 20.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Image(
+            modifier = Modifier
+                .size((GlobalValue.topBarHeight / density / 3 * 2).dp)
+                .clip(RoundedCornerShape(50))
+                .clickable { moveToBackScreen() },
+            painter = painterResource(id = R.drawable.arrow_back),
+            contentDescription = null
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "친구추가",
+                fontSize = 30.sp
+            )
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun AddFriendScreenTopBarPreview() {
+//    AddFriendScreenTopBar()
+//}
