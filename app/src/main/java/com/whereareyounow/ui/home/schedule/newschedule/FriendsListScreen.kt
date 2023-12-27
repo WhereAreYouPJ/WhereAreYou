5f065505af7f333cef4dc923c567168b258aabb5
CodingHarang <txepahs@gmail.com>
2023-12-27 23:30:52 +0900

    Update FriendsListScreen.kt

--- app/src/main/java/com/whereareyounow/ui/home/schedule/newschedule/FriendsListScreen.kt
+++ app/src/main/java/com/whereareyounow/ui/home/schedule/newschedule/FriendsListScreen.kt
@@ -1,9 +1,12 @@
 package com.whereareyounow.ui.home.schedule.newschedule
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -28,6 +32,7 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -35,6 +40,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
 import com.whereareyounow.R
 import com.whereareyounow.data.GlobalValue
 import com.whereareyounow.domain.entity.schedule.Friend
@@ -78,10 +85,24 @@ fun FriendsListScreen(
         }
         Spacer(Modifier.height(10.dp))
         LazyRow() {
-            itemsIndexed(selectedFriendsList) { index, friend ->
-                Text(
-                    text = friend.name
-                )
+            itemsIndexed(selectedFriendsList) { _, friend ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    GlideImage(
+                        modifier = Modifier
+                            .size(30.dp)
+                            .clip(RoundedCornerShape(50)),
+                        imageModel = { friend.profileImgUrl ?: R.drawable.account_circle_fill0_wght200_grad0_opsz24 },
+                        imageOptions = ImageOptions(
+                            contentScale = ContentScale.FillWidth,
+                        )
+                    )
+                    Text(
+                        text = friend.name
+                    )
+                }
+                Spacer(Modifier.width(10.dp))
             }
         }
         Spacer(Modifier.height(10.dp))
@@ -102,37 +123,29 @@ fun FriendsListScreen(
         BasicTextField(
             value = inputFriendName,
             onValueChange = { viewModel.updateInputText(it) },
-            textStyle = TextStyle(fontSize = 30.sp),
+            textStyle = TextStyle(fontSize = 20.sp),
             singleLine = true,
             decorationBox = {
-                Box(
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .drawBehind {
-                            val borderSize = 1.dp.toPx()
-                            drawLine(
-                                color = Color(0xFF858585),
-                                start = Offset(0f, size.height),
-                                end = Offset(size.width, size.height),
-                                strokeWidth = borderSize
-                            )
-                        },
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    it()
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.CenterEnd
-                    ) {
-                        Image(
-                            modifier = Modifier.clickable {
-//                                clearInputId()
-                            },
-                            painter = painterResource(id = R.drawable.baseline_cancel_24),
-                            contentDescription = null,
-                            colorFilter = ColorFilter.tint(color = Color(0xFF858585))
+                        .border(
+                            border = BorderStroke(
+                                width = 1.dp,
+                                color = Color(0xFF9B99AB)
+                            ),
+                            shape = RoundedCornerShape(50)
                         )
-                    }
+                        .padding(start = 20.dp, top = 10.dp, end = 20.dp, bottom = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.search_24px),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(color = Color(0xFF9B99AB))
+                    )
+                    Spacer(Modifier.width(10.dp))
+                    it()
                 }
             }
         )
@@ -144,22 +157,32 @@ fun FriendsListScreen(
             modifier = Modifier.weight(1f)
         ) {
             itemsIndexed(friendsList) { _, friend ->
-                Box(
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .clickable {
                             viewModel.selectFriend(friend)
                         }
-                        .padding(top = 10.dp, bottom = 10.dp)
+                        .padding(top = 10.dp, bottom = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
+                    GlideImage(
+                        modifier = Modifier
+                            .size(30.dp)
+                            .clip(RoundedCornerShape(50)),
+                        imageModel = { friend.first.profileImgUrl ?: R.drawable.account_circle_fill0_wght200_grad0_opsz24 },
+                        imageOptions = ImageOptions(
+                            contentScale = ContentScale.FillWidth,
+                        )
+                    )
+                    Spacer(Modifier.width(10.dp))
                     Text(
-                        modifier = Modifier.align(Alignment.CenterStart),
                         text = friend.first.name,
                         fontSize = 24.sp
                     )
+                    Spacer(Modifier.weight(1f))
                     Image(
                         modifier = Modifier
-                            .align(Alignment.CenterEnd)
                             .size(30.dp),
                         painter = when (friend.second) {
                             true -> painterResource(id = R.drawable.check_yellow)
@@ -180,8 +203,9 @@ fun FriendsListScreen(
         }
         Box(
             modifier = Modifier
+                .padding(bottom = 20.dp)
                 .fillMaxWidth()
-                .height(60.dp)
+                .height(80.dp)
                 .clip(RoundedCornerShape(10.dp))
                 .background(
                     color = Color(0xFF2A2550),
