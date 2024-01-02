8ccc418f2a6861500f88d8ecd27c6cd151fe64b3
CodingHarang <txepahs@gmail.com>
2024-01-03 01:50:27 +0900

    Update GlobalValue.kt

--- app/src/main/java/com/whereareyounow/data/GlobalValue.kt
+++ app/src/main/java/com/whereareyounow/data/GlobalValue.kt
@@ -3,18 +3,16 @@ package com.whereareyounow.data
 object GlobalValue {
 
     // 상단 상태바, 시스템 네비게이션 바 제외한 화면 높이
-    var screenHeightWithoutStatusBar = 1464f
+    var screenHeightWithoutStatusBar = 3116f
     // 화면 가로 너비
-    var screenWidth = 720f
+    var screenWidth = 1440f
     // 상단 영역 높이
-    var topBarHeight = 97.6f
+    var topBarHeight = 207f
     // 하단 네비게이션 바 높이
-    var bottomNavBarHeight = 97.6f
+    var bottomNavBarHeight = 207f
     // 캘린더 뷰 높이
-    var calendarViewHeight = 507.52f
+    var calendarViewHeight = 1080f
     // 일별 간략 정보 뷰 높이
-    var dailyScheduleViewHeight = 761.28f
-    // pixel <-> dp 전환에 사용할 밀도
-    var density = 1f
+    var dailyScheduleViewHeight = 1620f
     // 상단 상태바 높이
 }
