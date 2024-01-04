bf194719020c29aba1c63e4c5d61d7b25cf0c393
CodingHarang <txepahs@gmail.com>
2024-01-04 11:43:04 +0900

    Create Components.kt

---
+++ app/src/main/java/com/whereareyounow/ui/component/Components.kt
@@ -0,0 +1,54 @@
+package com.whereareyounow.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.whereareyounow.ui.theme.WhereAreYouTheme
+
+@Composable
+fun BottomOKButton(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color(0xFF2D2573))
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Medium
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun BottomOkButtonPreview() {
+    WhereAreYouTheme {
+        BottomOKButton(
+            text = "완료",
+            onClick = {  }
+        )
+    }
+}
