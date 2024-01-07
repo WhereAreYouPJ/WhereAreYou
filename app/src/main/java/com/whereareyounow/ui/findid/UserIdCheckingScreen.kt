2f9e661f3e33f4a5b4cb160ee8993c66b625f638
CodingHarang <txepahs@gmail.com>
2024-01-07 22:53:59 +0900

    Create UserIdCheckingScreen.kt

---
+++ app/src/main/java/com/whereareyounow/ui/findid/UserIdCheckingScreen.kt
@@ -0,0 +1,120 @@
+package com.whereareyounow.ui.findid
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.whereareyounow.R
+import com.whereareyounow.data.GlobalValue
+import com.whereareyounow.ui.component.BottomOKButton
+import com.whereareyounow.ui.component.CustomTopBar
+import com.whereareyounow.ui.theme.WhereAreYouTheme
+
+@Composable
+fun UserIdCheckingScreen(
+    moveToSignInScreen: () -> Unit,
+    viewModel: FindIdViewModel = hiltViewModel()
+) {
+    val searchedUserId = viewModel.searchedUserId.collectAsState().value
+    UserIdCheckingScreen(
+        searchedUserId = searchedUserId,
+        moveToSignInScreen = moveToSignInScreen
+    )
+}
+
+@Composable
+private fun UserIdCheckingScreen(
+    searchedUserId: String,
+    moveToSignInScreen: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 20.dp, end = 20.dp)
+    ) {
+        UserIdCheckingScreenTopBar(moveToSignInScreen)
+
+        Spacer(Modifier.height(20.dp))
+
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    SpanStyle(
+                        fontSize = 20.sp
+                    )
+                ) {
+                    append("해당 이메일과 연동된 아이디는 ")
+                }
+                withStyle(
+                    SpanStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append(searchedUserId)
+                }
+                withStyle(
+                    SpanStyle(
+                        fontSize = 20.sp
+                    )
+                ) {
+                    append(" 입니다.")
+                }
+            }
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        BottomOKButton(
+            text = "로그인하러 가기",
+            onClick = moveToSignInScreen
+        )
+
+        Spacer(Modifier.height(20.dp))
+    }
+}
+
+@Composable
+private fun UserIdCheckingScreenTopBar(
+    moveToSignInScreen: () -> Unit
+) {
+    CustomTopBar(
+        title = "아이디 찾기",
+        onBackButtonClicked = moveToSignInScreen
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun UserIdCheckingScreenPreview() {
+    WhereAreYouTheme {
+        UserIdCheckingScreen(
+            searchedUserId = "test",
+            moveToSignInScreen = {}
+        )
+    }
+}
