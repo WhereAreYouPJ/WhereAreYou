6d59f79dc8bd8421170ef01e9029590637d48596
CodingHarang <txepahs@gmail.com>
2024-01-07 11:33:17 +0900

    Create CustomTopBar.kt

---
+++ app/src/main/java/com/whereareyounow/ui/component/CustomTopBar.kt
@@ -0,0 +1,60 @@
+package com.whereareyounow.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.whereareyounow.R
+import com.whereareyounow.data.GlobalValue
+
+@Composable
+fun CustomTopBar(
+    title: String,
+    onBackButtonClicked: () -> Unit,
+) {
+    val density = LocalDensity.current.density
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height((GlobalValue.topBarHeight / density).dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .size((GlobalValue.topBarHeight / density / 2).dp)
+                .clip(RoundedCornerShape(50))
+                .clickable { onBackButtonClicked() },
+            painter = painterResource(id = R.drawable.arrow_back),
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomTopBarPreview() {
+    CustomTopBar(
+        title = "아이디 찾기",
+        onBackButtonClicked = {}
+    )
+}
