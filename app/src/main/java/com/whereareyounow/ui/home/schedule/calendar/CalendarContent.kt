9c82812c1bc8a3507a1cc7207d19334f130969cc
CodingHarang <txepahs@gmail.com>
2024-01-20 22:31:29 +0900

    Update CalendarContent.kt

--- app/src/main/java/com/whereareyounow/ui/home/schedule/calendar/CalendarContent.kt
+++ app/src/main/java/com/whereareyounow/ui/home/schedule/calendar/CalendarContent.kt
@@ -11,12 +11,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.whereareyounow.data.GlobalValue
+import com.whereareyounow.data.Schedule
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CalendarContent(
+    currentMonthCalendarInfo: List<Schedule>,
+    calendarState: CalendarViewModel.CalendarState,
+    updateCalendarState: (CalendarViewModel.CalendarState) -> Unit,
+    selectedYear: Int,
+    updateYear: (Int) -> Unit,
+    selectedMonth: Int,
+    updateMonth: (Int) -> Unit,
+    selectedDate: Int,
+    updateDate: (Int) -> Unit,
     state: AnchoredDraggableState<DetailState>,
 ) {
     val density = LocalDensity.current
@@ -24,8 +34,28 @@ fun CalendarContent(
     Box(
         modifier = Modifier.height(((GlobalValue.calendarViewHeight + state.offset) / density.density).dp)
     ) {
-        DateCalendar(expandDetailContent = { coroutineScope.launch(Dispatchers.Default) { state.animateTo(DetailState.Open) } })
-        MonthCalendar(expandDetailContent = { coroutineScope.launch(Dispatchers.Default) { state.animateTo(DetailState.Open) } })
-        YearCalendar()
+        DateCalendar(
+            currentMonthCalendarInfo = currentMonthCalendarInfo,
+            calendarState = calendarState,
+            selectedYear = selectedYear,
+            updateYear = updateYear,
+            selectedMonth = selectedMonth,
+            updateMonth = updateMonth,
+            selectedDate = selectedDate,
+            updateDate = updateDate,
+            expandDetailContent = { coroutineScope.launch(Dispatchers.Default) { state.animateTo(DetailState.Open) } }
+        )
+        MonthCalendar(
+            calendarState = calendarState,
+            updateMonth = updateMonth,
+            updateCalendarState = updateCalendarState,
+            expandDetailContent = { coroutineScope.launch(Dispatchers.Default) { state.animateTo(DetailState.Open) } }
+        )
+        YearCalendar(
+            selectedYear = selectedYear,
+            calendarState = calendarState,
+            updateYear = updateYear,
+            updateCalendarState = updateCalendarState
+        )
     }
 }
