da4ec72c90ef414755b3bdd09e4c47fbadecfe04
CodingHarang <txepahs@gmail.com>
2024-01-22 15:07:43 +0900

    Update YearCalendar.kt

--- app/src/main/java/com/whereareyounow/ui/home/schedule/calendar/YearCalendar.kt
+++ app/src/main/java/com/whereareyounow/ui/home/schedule/calendar/YearCalendar.kt
@@ -1,37 +1,42 @@
 package com.whereareyounow.ui.home.schedule.calendar
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.whereareyounow.ui.theme.WhereAreYouTheme
 import com.whereareyounow.util.AnimationUtil
 import com.whereareyounow.util.CalendarUtil
 
 @Composable
 fun YearCalendar(
-    viewModel: CalendarViewModel = hiltViewModel()
+    selectedYear: Int,
+    calendarState: CalendarViewModel.CalendarState,
+    updateYear: (Int) -> Unit,
+    updateCalendarState: (CalendarViewModel.CalendarState) -> Unit,
 ) {
-
-    val calendarState = viewModel.calendarState.collectAsState().value
-
     // 년도 선택 화면
     AnimatedVisibility(
         visible = calendarState == CalendarViewModel.CalendarState.YEAR,
         enter = AnimationUtil.enterTransition,
         exit = AnimationUtil.exitTransition
-
     ) {
         val listState = rememberLazyGridState()
         LazyVerticalGrid(
@@ -43,12 +48,18 @@ fun YearCalendar(
                     modifier = Modifier
                         .height(100.dp)
                         .clickable {
-                            viewModel.updateYear(year)
-                            viewModel.updateCalendarState(CalendarViewModel.CalendarState.MONTH)
+                            updateYear(year)
+                            updateCalendarState(CalendarViewModel.CalendarState.MONTH)
                         },
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
+                        modifier = Modifier
+                            .background(
+                                color = if (selectedYear == year) Color.Cyan else Color(0xFFFFFFFF),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .padding(10.dp),
                         text = "${year}년"
                     )
                 }
@@ -58,5 +69,18 @@ fun YearCalendar(
             val targetColumnIndex = 99
             listState.scrollToItem(targetColumnIndex)
         }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun YearCalendarPreview() {
+    WhereAreYouTheme {
+        YearCalendar(
+            selectedYear = 2024,
+            calendarState = CalendarViewModel.CalendarState.YEAR,
+            updateYear = {  },
+            updateCalendarState = {  }
+        )
     }
 }
