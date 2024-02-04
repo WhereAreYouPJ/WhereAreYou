582061db1dae7d46a6919315f8a4db2132c167a7
CodingHarang <txepahs@gmail.com>
2024-02-05 04:00:37 +0900

    Update Theme.kt

--- app/src/main/java/com/whereareyounow/ui/theme/Theme.kt
+++ app/src/main/java/com/whereareyounow/ui/theme/Theme.kt
@@ -1,14 +1,11 @@
 package com.whereareyounow.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -19,7 +16,7 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Color(0xFFFFFFFF)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
