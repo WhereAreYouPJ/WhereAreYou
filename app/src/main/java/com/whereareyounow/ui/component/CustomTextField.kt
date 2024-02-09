0278ea0caa17a2c092d6998dd449f507900544fe
CodingHarang <txepahs@gmail.com>
2024-02-09 10:27:46 +0900

    Update CustomTextField.kt

--- app/src/main/java/com/whereareyounow/ui/component/CustomTextField.kt
+++ app/src/main/java/com/whereareyounow/ui/component/CustomTextField.kt
@@ -1,10 +1,13 @@
 package com.whereareyounow.ui.component
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,10 +15,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,18 +35,29 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.whereareyounow.R
 import com.whereareyounow.ui.theme.WhereAreYouTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CustomTextField(
     hint: String,
     inputText: String,
     onValueChange: (String) -> Unit,
+    guideLine: String,
     textFieldState: CustomTextFieldState,
     isPassword: Boolean = false
 ) {
+    val viewRequester = BringIntoViewRequester()
+    val coroutineScope = rememberCoroutineScope()
     BasicTextField(
+        modifier = Modifier
+            .animateContentSize { _, _ -> }
+            .bringIntoViewRequester(viewRequester),
         value = inputText,
-        onValueChange = onValueChange,
+        onValueChange = {
+            onValueChange(it)
+            coroutineScope.launch { viewRequester.bringIntoView() }
+        },
         textStyle = TextStyle(
             color = Color(0xFF000000),
             fontSize = 16.sp
@@ -48,60 +65,57 @@ fun CustomTextField(
         singleLine = true,
         visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-//                .border(
-//                    border = BorderStroke(
-//                        width = 1.dp,
-//                        color = when (textFieldState) {
-//                            CustomTextFieldState.IDLE -> Color(0xFF808080)
-//                            CustomTextFieldState.SATISFIED -> Color(0xFF78C480)
-//                            CustomTextFieldState.UNSATISFIED -> Color(0xFFE59090)
-//                        }
-//                    ),
-//                    shape = RoundedCornerShape(10.dp)
-//                )
-                .background(
-                    color = Color(0xFFF5F5F6),
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(start = 10.dp, end = 10.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                modifier = Modifier.weight(1f),
-                contentAlignment = Alignment.CenterStart
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+//                    .height(50.dp)
+                    .background(
+                        color = Color(0xFFF5F5F6),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(start = 10.dp, top = 14.dp, end = 10.dp, bottom = 14.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                it()
-                if (inputText == "") {
+                Box(
+                    modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.CenterStart
+                ) {
                     Text(
-                        text = hint,
+                        text = if (inputText == "") hint else "",
                         fontSize = 16.sp,
                         color = Color(0xFFC1C1C1)
                     )
+                    it()
+                }
+                Spacer(Modifier.width(20.dp))
+                when (textFieldState) {
+                    CustomTextFieldState.SATISFIED -> {
+                        Image(
+                            modifier = Modifier.size(20.dp),
+                            painter = painterResource(id = R.drawable.check_circle_fill0_wght300_grad0_opsz24),
+                            contentDescription = null,
+                            colorFilter = ColorFilter.tint(color = Color(0xFF78C480))
+                        )
+                    }
+                    CustomTextFieldState.UNSATISFIED -> {
+                        Image(
+                            modifier = Modifier.size(20.dp),
+                            painter = painterResource(id = R.drawable.cancel_fill0_wght300_grad0_opsz24),
+                            contentDescription = null,
+                            colorFilter = ColorFilter.tint(color = Color(0xFFE59090))
+                        )
+                    }
+                    else -> {}
                 }
             }
-            Spacer(Modifier.width(20.dp))
-            when (textFieldState) {
-                CustomTextFieldState.SATISFIED -> {
-                    Image(
-                        modifier = Modifier.size(20.dp),
-                        painter = painterResource(id = R.drawable.check_circle_fill0_wght300_grad0_opsz24),
-                        contentDescription = null,
-                        colorFilter = ColorFilter.tint(color = Color(0xFF78C480))
-                    )
-                }
-                CustomTextFieldState.UNSATISFIED -> {
-                    Image(
-                        modifier = Modifier.size(20.dp),
-                        painter = painterResource(id = R.drawable.cancel_fill0_wght300_grad0_opsz24),
-                        contentDescription = null,
-                        colorFilter = ColorFilter.tint(color = Color(0xFFE59090))
-                    )
-                }
-                else -> {}
+            if (textFieldState == CustomTextFieldState.UNSATISFIED) {
+                Spacer(Modifier.height(10.dp))
+                Text(
+                    text = guideLine,
+                    fontSize = 14.sp,
+                    color = Color(0xFFFF0000)
+                )
             }
         }
     }
@@ -119,6 +133,7 @@ private fun IdleCustomTextFieldPreview() {
             hint = "Hint",
             inputText = "",
             onValueChange = {  },
+            guideLine = "GuideLine",
             textFieldState = CustomTextFieldState.IDLE
         )
     }
@@ -132,6 +147,7 @@ private fun IdlePasswordCustomTextFieldPreview() {
             hint = "Hint",
             inputText = "",
             onValueChange = {  },
+            guideLine = "GuideLine",
             textFieldState = CustomTextFieldState.IDLE,
             isPassword = true
         )
@@ -146,6 +162,7 @@ private fun SatisfiedCustomTextFieldPreview() {
             hint = "Hint",
             inputText = "Satisfied",
             onValueChange = {  },
+            guideLine = "GuideLine",
             textFieldState = CustomTextFieldState.SATISFIED
         )
     }
@@ -159,6 +176,7 @@ private fun SatisfiedPasswordCustomTextFieldPreview() {
             hint = "Hint",
             inputText = "Satisfied",
             onValueChange = {  },
+            guideLine = "GuideLine",
             textFieldState = CustomTextFieldState.SATISFIED,
             isPassword = true
         )
@@ -173,6 +191,7 @@ private fun UnsatisfiedCustomTextFieldPreview() {
             hint = "Hint",
             inputText = "Unsatisfied",
             onValueChange = {  },
+            guideLine = "GuideLine",
             textFieldState = CustomTextFieldState.UNSATISFIED
         )
     }
@@ -186,6 +205,7 @@ private fun UnsatisfiedPasswordCustomTextFieldPreview() {
             hint = "Hint",
             inputText = "Unsatisfied",
             onValueChange = {  },
+            guideLine = "GuideLine",
             textFieldState = CustomTextFieldState.UNSATISFIED,
             isPassword = true
         )
