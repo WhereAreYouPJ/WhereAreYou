689661b560fe8b95fdeacc5f2e317f3e99001d50
CodingHarang <txepahs@gmail.com>
2024-02-15 15:38:36 +0900

    ADD Test Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,20 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.datastore:datastore-core:1.0.0")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
+
+
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.2.1")
+    testImplementation("com.google.truth:truth:1.0.1")
+    testImplementation("androidx.test.ext:junit:1.1.2")
+    testImplementation("org.robolectric:robolectric:4.4")
+
+    androidTestImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.2.1")
+    androidTestImplementation("com.google.truth:truth:1.0.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
