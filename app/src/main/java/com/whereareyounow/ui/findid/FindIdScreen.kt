8f4bd0300308daa1918145f5c8b6c233f2b9823a
CodingHarang <txepahs@gmail.com>
2024-02-24 14:15:26 +0900

    CHORE

--- app/src/main/java/com/whereareyounow/ui/findid/FindIdScreen.kt
+++ app/src/main/java/com/whereareyounow/ui/findid/FindIdScreen.kt
@@ -1,6 +1,7 @@
 package com.whereareyounow.ui.findid
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
