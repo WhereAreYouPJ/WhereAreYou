484a785df31ec3a13cdae1ef1099462571e78c64
CodingHarang <txepahs@gmail.com>
2024-02-26 11:14:51 +0900

    MODIFY DetailScheduleMapScreenUIState

--- app/src/main/java/com/whereareyounow/data/detailschedule/DetailScheduleMapScreenUIState.kt
+++ app/src/main/java/com/whereareyounow/data/detailschedule/DetailScheduleMapScreenUIState.kt
@@ -3,5 +3,5 @@ package com.whereareyounow.data.detailschedule
 data class DetailScheduleMapScreenUIState(
     val destinationLatitude: Double = 0.0,
     val destinationLongitude: Double = 0.0,
-    val memberLocationsList: List<MemberLocation> = emptyList()
+    val memberInfosList: List<MemberInfo> = emptyList()
 )
