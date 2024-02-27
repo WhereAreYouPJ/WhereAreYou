0da1da45d5e16737d62625daa884d7587653946e
CodingHarang <txepahs@gmail.com>
2024-02-28 02:18:09 +0900

    ADD FindPasswordScreenUIState

---
+++ app/src/main/java/com/whereareyounow/data/findpw/FindPasswordScreenUIState.kt
@@ -0,0 +1,19 @@
+package com.whereareyounow.data.findpw
+
+data class FindPasswordScreenUIState(
+    val inputUserId: String = "",
+    val inputEmail: String = "",
+    val inputEmailState: EmailState = EmailState.EMPTY,
+    val inputVerificationCode: String = "",
+    val inputVerificationCodeState: VerificationCodeState = VerificationCodeState.EMPTY,
+    val isVerificationCodeSent: Boolean = false,
+    val emailVerificationLeftTime: Int = 0
+)
+
+enum class EmailState {
+    EMPTY, SATISFIED, UNSATISFIED
+}
+
+enum class VerificationCodeState {
+    EMPTY, SATISFIED, UNSATISFIED
+}
