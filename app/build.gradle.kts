8157fed30dfb9cc2f329536778a342e549fec100
CodingHarang <txepahs@gmail.com>
2024-02-29 09:32:08 +0900

    MODIFY Build Version Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.whereareyounow"
         minSdk = 24
         targetSdk = 34
-        versionCode = 33
+        versionCode = 36
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
