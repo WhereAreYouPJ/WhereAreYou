fa174914afb905f3e8860e49ce8b8a1e5817bfad
CodingHarang <txepahs@gmail.com>
2024-02-29 09:32:38 +0900

    ADD ScheduleInvitationInfo

---
+++ app/src/main/java/com/whereareyounow/data/notification/ScheduleInvitationInfo.kt
@@ -0,0 +1,12 @@
+package com.whereareyounow.data.notification
+
+data class ScheduleInvitationInfo(
+    val scheduleId: String,
+    val title: String,
+    val inviterUserName: String,
+    val year: String,
+    val month: String,
+    val date: String,
+    val hour: String,
+    val minute: String
+)
