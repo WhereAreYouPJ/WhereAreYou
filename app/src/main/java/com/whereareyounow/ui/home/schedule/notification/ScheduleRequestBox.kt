e1903d2ceb9414ed0a12db98355b9f20f7b31703
CodingHarang <txepahs@gmail.com>
2024-03-06 12:37:45 +0900

    CHORE

--- app/src/main/java/com/whereareyounow/ui/home/schedule/notification/ScheduleRequestBox.kt
+++ app/src/main/java/com/whereareyounow/ui/home/schedule/notification/ScheduleRequestBox.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -27,7 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.whereareyounow.R
-import com.whereareyounow.domain.entity.apimessage.schedule.ScheduleInvitation
+import com.whereareyounow.data.notification.ScheduleInvitationInfo
 
 @Composable
 fun ScheduleRequestBox(
@@ -112,7 +111,7 @@ fun ScheduleRequestBox(
                 )
                 Spacer(Modifier.width(10.dp))
                 Text(
-                    text = "${scheduleRequest.userName}",
+                    text = "${scheduleRequest.inviterUserName}",
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Medium,
                     maxLines = 1,
@@ -153,7 +152,7 @@ private fun ScheduleRequestBoxPreview() {
         scheduleRequest = ScheduleInvitationInfo(
             scheduleId = "",
             title = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            userName = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+            inviterUserName = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
             year = "2023",
             month = "12",
             date = "12",
--- app/src/main/java/com/whereareyounow/ui/signup/PolicyAgreeScreen.kt
+++ app/src/main/java/com/whereareyounow/ui/signup/PolicyAgreeScreen.kt
@@ -2,22 +2,16 @@ package com.whereareyounow.ui.signup
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,11 +21,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -39,7 +31,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.whereareyounow.R
-import com.whereareyounow.data.GlobalValue
 import com.whereareyounow.ui.component.BottomOKButton
 import com.whereareyounow.ui.component.CustomTopBar
 
@@ -65,7 +56,7 @@ fun PolicyAgreeScreen(
         Spacer(Modifier.height(20.dp))
 
         Text(
-            text = "약관에 동의하시면\n회원가입이 완료됩니다",
+            text = "회원가입에 필요한\n약관에 동의해주세요",
             fontSize = 26.sp,
             fontWeight = FontWeight.Bold,
             style = TextStyle(
